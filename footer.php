2d90a7614ebc9a081e4cd8b242e1f39860663932
Pavel <silvesterkot@gmail.com>
2020-07-08 12:18:27 +1000

    first commit

---
+++ footer.php
@@ -0,0 +1,17 @@
+  <footer class="footer">
+    <div class="container">
+      <a href="tel:<?php the_field ('phone') ?>" class="phone"><?php the_field ('phone') ?>
+      </a>
+      <div class="footer__email">
+        namename@gmail.com
+      </div>
+    </div>
+
+  </footer>
+
+<?php wp_footer(); ?>
+
+</body>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,136 @@
+<?php get_header(); ?>
+
+  <main class="main">
+    <div class="container">
+      <div class="main__title">
+        Строительство домов и бань из
+        строганного и профилированного бруса
+      </div>
+      <div class="main__text">
+        За 10 лет мы возвели больше 250 домов и бань из бруса
+
+        ИМЕЕМ БОЛЬШОЙ ОПЫТ В СТРОИТЕЛЬСТВЕ
+        гарантируем качество и надежность всех построенных нами объектов
+
+        ГАРАНТИЯ НА ДОМА И БАНИ ИЗ БРУСА 3 ГОДА
+      </div>
+      <div class="main__img">
+        <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/main-img.png" alt="" class="">
+      </div>
+
+      <div class="project">
+        <div class="project__title">
+          ПРОЕКТЫ ДОМОВ ИЗ БРУСА
+        </div>
+
+        <div class="project__item">
+          <div class="project__name">
+            ДОМ #1
+          </div>
+          <div class="project__size">
+            Размер дома: 6 x 6
+          </div>
+          <div class="project__area">
+            Общая площадь дома: 54 кв. м
+          </div>
+          <div class="project__price">
+            Стоимость - 402 000 рублей
+          </div>
+          <div class="project__images">
+            <div class="project__images-item1">
+              <img src="img/project-1.jpg" alt="">
+            </div>
+            <div class="project__images-item">
+              <img src="img/project-2.jpg" alt="">
+            </div>
+          </div>
+        </div>
+
+        <div class="project__item">
+          <div class="project__name">
+            ДОМ #2
+          </div>
+          <div class="project__size">
+            Размер дома: 6 x 7.5
+          </div>
+          <div class="project__area">
+            Общая площадь дома: 72 кв. м
+          </div>
+          <div class="project__price">
+            Стоимость - 457 000 рублей
+          </div>
+          <div class="project__images">
+            <div class="project__images-item1">
+              <img src="img/project-3.jpg" alt="">
+            </div>
+            <div class="project__images-item">
+              <img src="img/project-4.jpg" alt="">
+            </div>
+          </div>
+        </div>
+
+        <div class="project__item">
+          <div class="project__name">
+            ДОМ #3
+          </div>
+          <div class="project__size">
+            Размер дома: 8 x 7.5
+          </div>
+          <div class="project__area">
+            Общая площадь дома: 92 кв. м
+          </div>
+          <div class="project__price">
+            Стоимость - 635 000 рублей
+          </div>
+          <div class="project__images">
+            <div class="project__images-item1">
+              <img src="img/project-5.jpg" alt="">
+            </div>
+            <div class="project__images-item">
+              <img src="img/project-6.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="download">
+      <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/home-download.png" alt="">
+      <div class="download__link">
+        <a href="#">СКАЧАТЬ ВЕСЬ КАТАЛОГ ДОМОВ</a>
+
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="gallery">
+
+        <div class="gallery__title">
+          ФОТОГРАФИИ НАШИХ РАБОТ
+        </div>
+        <div class="gallery__text">
+          некоторые фотографии построенных нами домов и бань из бруса
+        </div>
+        <div class="gallery__inner">
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+          <a href="img/item-1.jpg"><img src="img/item-1.jpg" alt=""></a>
+        </div>
+      </div>
+
+      <div class="main__img">
+        <img src="<?php bloginfo( 'template_url' ); ?>/assets/img/main-img.png" alt="">
+      </div>
+    </div>
+  </main>
+
+<?php get_footer(); ?>
